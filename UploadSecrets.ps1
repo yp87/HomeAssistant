27beec10dd94d8f6d5a8159d85063dc773f1e07e
Yan <yan.paquette@gmail.com>
2020-08-28 14:03:23 -0400

    Add powershell script to copy secrets to home assistant host

---
+++ UploadSecrets.ps1
@@ -0,0 +1,12 @@
+Import-Module Microsoft.PowerShell.SecretManagement
+
+$s = New-PSSession -HostName (Get-Secret HomeAssistantHost -AsPlainText) -UserName (Get-Secret HomeAssistantUsername -AsPlainText)#(New-Object PSCredential "$(Get-Secret HomeAssistantUsername -AsPlainText),$(Get-Secret HomeAssistantPassword -AsPlainText)")
+
+$SourcePath = Get-Location;
+Get-Childitem -Path "$($SourcePath)/**/secret*.yaml" -Recurse |
+ForEach-Object {
+  $childPath = "$_".substring("$($sourcePath)".length+1)
+  $dest = "$(Get-Secret HomeAssistantConfigPath -AsPlainText)\$($childPath)"
+  Invoke-Command -Session $s -ScriptBlock { New-Item -Path $args[0] -type File -Force } -ArgumentList $dest
+  Copy-Item -Verbose -Path $_ -Destination $dest -Force -ToSession $s
+}
