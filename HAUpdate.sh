05f24acbe2bd7d1e0ff983e005104610e7fcba57
Yan <yan.paquette@gmail.com>
2020-08-29 18:25:16 -0400

    clean unused containers and volumes whe ndeploying

--- HAUpdate.sh
+++ HAUpdate.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 docker-compose pull
-docker-compose down
-docker-compose up -d
-docker system prune -fa
-docker volume prune -f
+./deploy.sh
--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+# moving to script directory regardless of the execution path
 cd "${0%/*}"
+
 docker-compose down
 docker-compose up -d
+docker system prune -fa
+docker volume prune -f
+
