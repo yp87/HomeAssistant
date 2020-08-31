575128929e459280e3d676d8ed0ab6a93fbd249e
Yan <yan.paquette@gmail.com>
2020-08-31 19:41:59 -0400

    The supervisor now receives webhook calls from github

--- WebhookProxy/run.sh
+++ WebhookProxy/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 smee_url="`cat /WebhookProxy/smeeurl`"
-smee -u $smee_url -t http://host.docker.internal:3000/automation/webhook
+smee -u $smee_url -t http://supervisor:3000/automation/webhook
