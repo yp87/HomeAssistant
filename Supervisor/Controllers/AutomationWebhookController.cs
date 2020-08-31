382edbe1351b771e2b82f8c13f41bb1e8e5d4352
Yan <yan.paquette@gmail.com>
2020-08-30 22:27:59 -0400

    Add skeleton automation supervisor

---
+++ Supervisor/Controllers/AutomationWebhookController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Supervisor.Controllers
+{
+    [ApiController]
+    [Route("Automation")]
+    public class AutomationWebhookController : ControllerBase
+    {
+        [Route("Webhook")]
+        [HttpPost()]
+        public IActionResult ReceiveWebhook()
+        {
+            return Ok();
+        }
+    }
+}
