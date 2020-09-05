3e30ef475360d36e63f1a38986790fe8ccd7db1c
Yan <yan.paquette@gmail.com>
2020-09-04 22:17:53 -0400

    fix GitHubAction model

--- Supervisor/src/Models/GitHubAction.cs
+++ Supervisor/src/Models/GitHubAction.cs
@@ -1,9 +1,12 @@
+using System.Text.Json.Serialization;
+
 namespace Supervisor.Models
 {
     public class GitHubAction
     {
         public string? Action { get; set; }
 
+        [JsonPropertyName("check_run")]
         public CheckRun? CheckRun { get; set; }
 
         public Repository? Repository { get; set; }
