feb73db3b0afec6f8cb2cbe77c2b995b50a3e837
Yan <yan.paquette@gmail.com>
2020-09-13 08:51:30 -0400

    add service_account.json to upload secrets script

--- UploadSecrets.ps1
+++ UploadSecrets.ps1
@@ -1,12 +1,13 @@
 Import-Module Microsoft.PowerShell.SecretManagement
 
-$s = New-PSSession -HostName (Get-Secret HomeAssistantHost -AsPlainText) -UserName (Get-Secret HomeAssistantUsername -AsPlainText)#(New-Object PSCredential "$(Get-Secret HomeAssistantUsername -AsPlainText),$(Get-Secret HomeAssistantPassword -AsPlainText)")
+$s = New-PSSession -HostName (Get-Secret HomeAssistantHost -AsPlainText) -UserName (Get-Secret HomeAssistantUsername -AsPlainText)
 
-$SourcePath = Get-Location;
-Get-Childitem -Path "$($SourcePath)/**/secret*" -Recurse |
-ForEach-Object {
-  $childPath = "$_".substring("$($sourcePath)".length+1)
+$sourcePath = Get-Location;
+$secretFiles = Get-Childitem -Path "$($sourcePath)/**/secret*" -Recurse;
+$secretFiles += Get-Childitem -Path "$($sourcePath)/**/service_account.json" -Recurse
+Foreach($secretFile in $secretFiles) {
+  $childPath = "$secretFile".substring("$($sourcePath)".length+1)
   $dest = "$(Get-Secret HomeAssistantConfigPath -AsPlainText)\$($childPath)"
   Invoke-Command -Session $s -ScriptBlock { New-Item -Path $args[0] -type File -Force } -ArgumentList $dest
-  Copy-Item -Path $_ -Destination $dest -Force -ToSession $s
+  Copy-Item -Path $secretFile -Destination $dest -Force -ToSession $s
 }
