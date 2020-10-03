b60bb4beb12cb09c1395fe283b1886aa767b1ddc
Yan <yan.paquette@gmail.com>
2020-10-03 14:31:14 -0400

    Add .env path to commands that runs outside of docker-compose directory

--- HAUpdate.sh
+++ HAUpdate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker exec supervisor docker-compose -f /Source/docker-compose-homeassistant.yaml pull
+docker exec supervisor docker-compose -f /Source/docker-compose-homeassistant.yaml --env-file /Source/.env pull
 ./deploy.sh
--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker exec supervisor docker-compose -f /Source/docker-compose-homeassistant.yaml up -d --build
+docker exec supervisor docker-compose -f /Source/docker-compose-homeassistant.yaml --env-file /Source/.env up -d --build
 docker system prune -fa
 docker volume prune -f
