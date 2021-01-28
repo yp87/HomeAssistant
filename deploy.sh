ac2556107aa6061d724b5384a9f0cefdfead7693
Yan <yan.paquette@gmail.com>
2021-01-28 12:47:34 -0500

    ask to cleanup docker containers after update

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 docker exec supervisor docker-compose -f /Source/docker-compose-homeassistant.yaml --env-file /Source/.env up -d --build
-docker system prune -fa
-docker volume prune -f
+
+echo "Do you wish to cleanup all unused images and volumes?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) docker system prune -fa; docker volume prune -f; break;;
+        No ) exit;;
+    esac
+done
