ec6da0bf79fb8aba84888d32997b210ad68c0472
Yan <yan.paquette@gmail.com>
2022-06-16 21:03:12 -0400

    fx scripts

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker exec supervisor docker compose -f /Source/docker-compose-homeassistant.yaml --env-file /Source/.env up -d --build
+docker exec supervisor docker-compose -f /Source/docker-compose-homeassistant.yaml --env-file /Source/.env up -d --build
 
 echo "Do you wish to cleanup all unused images and volumes?"
 select yn in "Yes" "No"; do
